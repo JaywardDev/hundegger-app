b0a79b57e23cd98fbf48a448365b4c42fe6ad9f0
Jayward Severino <jaywardseverino.nz@gmail.com>
2025-11-03 08:11:41 +1300

    updated timber directory

--- src/lib/presets.ts
+++ src/lib/presets.ts
@@ -6,6 +6,7 @@ export const SIZE_PRESETS = [
   { id: "64x200", width_mm: 200, thickness_mm: 64 },
   { id: "64x240", width_mm: 240, thickness_mm: 64 },
   { id: "64x300", width_mm: 300, thickness_mm: 64 },
+  { id: "64x400", width_mm: 400, thickness_mm: 64 },
   { id: "64x600", width_mm: 600, thickness_mm: 64 },
   { id: "90x150", width_mm: 150, thickness_mm: 90 },
   { id: "90x200", width_mm: 200, thickness_mm: 90 },
@@ -18,8 +19,11 @@ export const SIZE_PRESETS = [
   { id: "135x300", width_mm: 300, thickness_mm: 135 },
   { id: "135x400", width_mm: 400, thickness_mm: 135 },
   { id: "180x240", width_mm: 240, thickness_mm: 180 },
+  { id: "180x300", width_mm: 300, thickness_mm: 180 },
   { id: "180x360", width_mm: 360, thickness_mm: 180 },
   { id: "180x400", width_mm: 400, thickness_mm: 180 },
+  { id: "180x460", width_mm: 460, thickness_mm: 180 },
+  { id: "225x460", width_mm: 460, thickness_mm: 225 },
 ];
 export const LENGTH_PRESETS_MM = [12000, 6000, 8900];
 
